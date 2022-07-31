943a637169e2540e88e6d6fa03d311a6924cd4dc
Viktor Zatorskyi <victor.zatorski@gmail.com>
2022-07-30 23:37:03 -0700

    Added: extensive logging of errors

--- ImageSimilarityIndex/__init__.py
+++ ImageSimilarityIndex/__init__.py
@@ -32,10 +32,8 @@ async def main(req: func.HttpRequest) -> func.HttpResponse:
         try:
             req_body = req.get_json()
             # Check JSON structure
-            if req_body['image_a'] == None or \
-               req_body['image_b'] == None or \
-               len(req_body['image_a']) == 0 or \
-               len(req_body['image_b']) == 0:
+            if req_body['image_a'] == None or req_body['image_b'] == None or \
+               len(req_body['image_a']) == 0 or len(req_body['image_b']) == 0:
                 raise "Wrong JSON structure"
         except:
             raise ValueError("Can't parse request JSON body")
@@ -54,7 +52,11 @@ async def main(req: func.HttpRequest) -> func.HttpResponse:
 
         async with model_lock:
             inference_start_time = time.time()
-            sim_score = await eventloop.run_in_executor(None, model.calculate, img, img2)
+            try:
+                sim_score = await eventloop.run_in_executor(None, model.calculate, img, img2)
+            except Exception as e:
+                logger.error(str(e))
+                raise ValueError("Can't compare images")
             inference_end_time = time.time()
 
         inference_delta = inference_end_time - inference_start_time
@@ -76,19 +78,25 @@ def process_url_attachment(url: str) -> Image:
         response = requests.get(
             url, stream=True, timeout=IMG_FETCH_TIMEOUT_SEC)
     except Exception as e:
+        logger.error(f"URL: {url[:200]}")
+        logger.error(str(e))
         raise ValueError("Can't download image")
 
     if response.status_code != 200:
+        logger.error(f"URL: {url[:200]}")
+        logger.error(f"Response code: {response.status_code}")
         raise ValueError("Can't download image")
 
     content = response.raw.read(MAX_REQUEST_LENGTH + 1)
     if len(content) > MAX_REQUEST_LENGTH:
+        logger.error(f"URL: {url[:200]}")
         raise ValueError(
             "Image file is bigger than allowed limit ({MAX_REQUEST_LENGTH} bytes)")
 
     try:
         return Image.open(io.BytesIO(content)).convert("RGB")
-    except:
+    except Exception as e:
+        logger.error(str(e))
         raise ValueError("Can't read downloaded image")
 
 
@@ -97,5 +105,6 @@ def process_b64_attachment(content: str) -> Image:
         base64_decoded = base64.b64decode(content.split(',')[1])
         img = Image.open(io.BytesIO(base64_decoded)).convert("RGB")
         return img
-    except:
+    except Exception as e:
+        logger.error(str(e))
         raise ValueError("Can't parse base64 image data")
--- TODO.md
+++ TODO.md
@@ -1,4 +1,3 @@
-* add more logging
 * make image loading parallel
 * try Pytorch JIT
 * try ONNX 
